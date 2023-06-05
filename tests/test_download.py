dddfcc67d09dc4299f7c8e28bf867a6a91dbb6c7
yuanmingqi <friedrichyuan19990827@gmail.com>
2023-06-05 19:18:13 +0800

    update arch.

--- tests/test_download.py
+++ tests/test_download.py
@@ -5,7 +5,7 @@ curren_dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(curren_dir_path, os.pardir))
 sys.path.append(parent_dir_path)
 
-from hsuanwuhub.datasets import Procgen
+from hub.datasets import Procgen
 
 if __name__ == "__main__":
     procgen = Procgen()
